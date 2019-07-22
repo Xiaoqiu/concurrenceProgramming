870c720ead36b986e6410383ea35e5220684fcdf
kate huang <495157297@qq.com>
2019-07-22 17:06:54 +0800

    update

--- demo/src/main/java/chapter4/Section4_3_2.java
+++ demo/src/main/java/chapter4/Section4_3_2.java
@@ -16,10 +16,11 @@ public class Section4_3_2 {
 
   /**
    * 线程池方法
-   * shutdownNow() : 不阻塞，主线程（main）马上结束,
-   * 线程池会继续运行直到所有任务执行完才会停止，不再添加新任务。
+   * shutdownNow() : 中断所有任务，并抛出InterruptedException异常
    */
 
+  public void test() {
 
+  }
 
 }
